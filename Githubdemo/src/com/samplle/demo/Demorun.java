7b7a81ce943c6ef9406449eb1cdfee84ede72eb2
Naveen <Naveen@DESKTOP-NSU4I27>
2022-06-07 15:33:12 +0530

    Basic commit

---
+++ Githubdemo/src/com/samplle/demo/Demorun.java
@@ -0,0 +1,12 @@
+package com.samplle.demo;
+
+public class Demorun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		System.out.println("Naveen Kumar Qa tester");
+		
+	}
+
+}
