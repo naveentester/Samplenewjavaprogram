6dd07f03fd7d355d5ef33985408bf957700cf50e
Naveen <Naveen@DESKTOP-NSU4I27>
2022-06-07 17:36:40 +0530

    Naveen Qa testing aadded

--- Githubdemo/src/com/samplle/demo/Demorun.java
+++ Githubdemo/src/com/samplle/demo/Demorun.java
@@ -6,7 +6,7 @@ public class Demorun {
 		// TODO Auto-generated method stub
         
 		System.out.println("Naveen Kumar Qa tester");
-		System.out.println("Naveen godb");
+		System.out.println("Naveen godb Qa testing");
 		
 	}
 
